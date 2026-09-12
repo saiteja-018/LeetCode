ff0a75dd201930284ebbd93a3cf46d7e678d95db
SATYAMSETTI SAI TEJA <saitejasatyamsetti@gmail.com>
2026-09-12 15:54:51 +0530

    Time: 1 ms (24.39%), Space: 8.7 MB (14.35%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low=0;
+        int high=x;
+        int ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if((long long)mid*mid<=x){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
