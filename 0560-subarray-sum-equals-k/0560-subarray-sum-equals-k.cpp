825b9168996e698e7623ed8451558cb3aaf8c7cb
SATYAMSETTI SAI TEJA <saitejasatyamsetti@gmail.com>
2026-09-22 21:59:59 +0530

    Time: 36 ms (80.05%), Space: 45.5 MB (38.51%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
+        unordered_map<int,int> mpp;
+        int sum=0;
+        int ans=0;
+        mpp[0]=1;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(mpp.find(sum-k)!=mpp.end()){
+                ans+=mpp[sum-k];
+            }
+            mpp[sum]++;
+        }
+
+    return ans;
+    }
+};
